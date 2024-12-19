fc6ea7fdab548a16246e9efff1958d1af0ea2d0c
olbo1973 <o.boldyreff@gmail.com>
2024-12-19 23:48:28 +0300

    19.12/1

---
+++ my_homeworks/module_9/module_9_3.py
@@ -0,0 +1,16 @@
+## Домашнее задание по теме "Генераторные сборки"
+
+
+# Даны два списка строк
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+# 1. Создание генераторной сборки, которая высчитывает разницу длин строк, если их длины не равны
+first_result = (abs(len(f) - len(s)) for f, s in zip(first, second) if len(f) != len(s))
+
+# 2. Генераторная сборка, которая содержит результаты сравнения длин строк в одинаковых позициях
+second_result = (len(first[i]) != len(second[i]) for i in range(len(first)))
+
+# Вывод результатов
+print(list(first_result))   # Ожидается: [1, 2]
+print(list(second_result))  # Ожидается: [False, False, True]
